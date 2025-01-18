58740a54b35b6a402233ae41284bfbaf17ae2ae4
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2025-01-18 17:30:32 +0100

    Add logo

--- assets/js/navbar-footer.js
+++ assets/js/navbar-footer.js
@@ -6,7 +6,7 @@ function loadNavbar() {
         <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
             <div class="container-fluid">
                 <a class="navbar-brand" href="index.html">
-                    <img src="assets/images/logo-light.png" id="logo" alt="Logo" class="logo-img" style="width: 120px;">
+                    <img src="assets/images/logo-img.png" id="logo" alt="Logo" class="logo-img" style="width: 100px;">
                 </a>
                 <button class="navbar-toggler" tabindex="0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
